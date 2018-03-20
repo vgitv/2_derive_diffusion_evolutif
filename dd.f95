ba7272619cd9d8912655bacb47ef0f86ec76762b
vgitv <valentin.mr@outlook.fr>
2018-03-20 10:36:34 +0100

    sch semi implicite done

--- dd.f95
+++ dd.f95
@@ -314,7 +314,7 @@ contains
     ! psi_r : CL droit
     ! B : fonction schéma parmi B1, B2, B3
     ! A : matrice retournée
-    subroutine build_A_si(t_courant, dt, m, psi, psi_l, psi_r, B, A)
+    subroutine build_A_N(t_courant, dt, m, psi, psi_l, psi_r, B, A)
         ! paramètres
         real(rp) :: t_courant
         real(rp) :: dt
@@ -326,6 +326,8 @@ contains
 
         ! variables locales
         integer :: i
+
+        A = 0.0_rp
 
         A(1, 1) = m%h(1) / dt + B( psi(1)-psi(2) ) / m%h2(2) + B( psi(1)-psi_l(t_courant) ) / m%h2(1)
         A(1, 2) = -B( psi(2)-psi(1) ) / m%h2(2)
@@ -342,6 +344,46 @@ contains
 
 
     ! -------------------------------------------------------------------------------------------------------
+    ! Matrice A_N (A semi implicite)
+    ! -------------------------------------------------------------------------------------------------------
+    ! t_courant : temps donné
+    ! dt : pas de temps
+    ! m : maillage d'espace
+    ! psi : vecteur itéré kième
+    ! psi_l : fonction CL bord gauche
+    ! psi_r : CL droit
+    ! B : fonction schéma parmi B1, B2, B3
+    ! A : matrice retournée
+    subroutine build_A_P(t_courant, dt, m, psi, psi_l, psi_r, B, A)
+        ! paramètres
+        real(rp) :: t_courant
+        real(rp) :: dt
+        type(Mesh), intent(in) :: m
+        real(rp), dimension(m%l), intent(in) :: psi
+        real(rp), external :: psi_l, psi_r
+        real(rp), external :: B
+        real(rp), dimension(m%l, m%l), intent(out) :: A
+
+        ! variables locales
+        integer :: i
+
+        A = 0.0_rp
+
+        A(1, 1) = m%h(1) / dt + B( -psi(1)+psi(2) ) / m%h2(2) + B( -psi(1)+psi_l(t_courant) ) / m%h2(1)
+        A(1, 2) = -B( -psi(2)+psi(1) ) / m%h2(2)
+        A(2, 1) = -B( -psi(1)+psi(2) ) / m%h2(2)
+        do i = 2, m%l - 1
+            A(i, i) = m%h(i) / dt + B( -psi(i)+psi(i+1) ) / m%h2(i+1) + B( -psi(i)+psi(i-1) ) / m%h2(i)
+            A(i, i+1) = -B( -psi(i+1)+psi(i) ) / m%h2(i+1)
+            A(i+1, i) = -B( -psi(i)+psi(i+1) ) / m%h2(i+1)
+        end do
+        A(m%l, m%l) = m%h(m%l) / dt + B( -psi(m%l)+psi_r(t_courant) ) / m%h2(m%l+1) + &
+            B( -psi(m%l)+psi(m%l-1) ) / m%h2(m%l)
+    end subroutine
+
+
+
+    ! -------------------------------------------------------------------------------------------------------
     ! Matrice b_N (b semi implicite)
     ! -------------------------------------------------------------------------------------------------------
     ! t_courant : temps donné
@@ -352,13 +394,13 @@ contains
     ! n_r : CL droit
     ! B : fonction schéma parmi B1, B2, B3
     ! b_N : matrice retournée
-    subroutine build_b_si(t_courant, dt, m, n, n_l, n_r, B, b_N)
+    subroutine build_b_N(t_courant, dt, m, psi, n, psi_l, psi_r, n_l, n_r, B, b_N)
         ! paramètres
         real(rp), intent(in) :: t_courant
         real(rp), intent(in) :: dt
         type(Mesh), intent(in) :: m
-        real(rp), dimension(m%l), intent(in) :: n
-        real(rp), external :: n_l, n_r
+        real(rp), dimension(m%l), intent(in) :: psi, n
+        real(rp), external :: psi_l, psi_r, n_l, n_r
         real(rp), external :: B
         real(rp), dimension(m%l), intent(out) :: b_N
 
@@ -366,8 +408,43 @@ contains
         integer :: i
 
         do i = 1, m%l
-            !++!
+            b_N(i) = m%h(i) * n(i) / dt
         end do
+        b_N(1) = b_N(1) + B(psi_l(t_courant)-psi(1)) / m%h2(1)
+        b_N(m%l) = b_N(m%l) + B(psi_r(t_courant)-psi(m%l)) / m%h2(m%l+1)
+    end subroutine
+
+
+
+    ! -------------------------------------------------------------------------------------------------------
+    ! Matrice b_P (b semi implicite)
+    ! -------------------------------------------------------------------------------------------------------
+    ! t_courant : temps donné
+    ! dt : pas de temps
+    ! m : maillage d'espace
+    ! n : vecteur itéré kième
+    ! n_l : fonction CL bord gauche
+    ! n_r : CL droit
+    ! B : fonction schéma parmi B1, B2, B3
+    ! b_N : matrice retournée
+    subroutine build_b_P(t_courant, dt, m, psi, p, psi_l, psi_r, p_l, p_r, B, b_P)
+        ! paramètres
+        real(rp), intent(in) :: t_courant
+        real(rp), intent(in) :: dt
+        type(Mesh), intent(in) :: m
+        real(rp), dimension(m%l), intent(in) :: psi, p
+        real(rp), external :: psi_l, psi_r, p_l, p_r
+        real(rp), external :: B
+        real(rp), dimension(m%l), intent(out) :: b_P
+
+        ! variables locales
+        integer :: i
+
+        do i = 1, m%l
+            b_P(i) = m%h(i) * p(i) / dt
+        end do
+        b_P(1) = b_P(1) + B(-psi_l(t_courant)+psi(1)) / m%h2(1)
+        b_P(m%l) = b_P(m%l) + B(-psi_r(t_courant)+psi(m%l)) / m%h2(m%l+1)
     end subroutine
 
 
@@ -400,8 +477,8 @@ contains
         real(rp) :: dt
         integer :: i
         real(rp), dimension(m%l) :: n_suiv, p_suiv, psi_suiv, ci
-        real(rp), dimension(m%l, m%l) :: A_N
-        real(rp), dimension(m%l) :: b_N
+        real(rp), dimension(m%l, m%l) :: A_N, A_P
+        real(rp), dimension(m%l) :: b_N, b_P
 
         dt = t(2) - t(1)
 
@@ -417,10 +494,14 @@ contains
 
         do i = 1, size(t)
             ! calcul itéré n suivant
-            call iter_n(t(i), dt, m, n, n_r, n_l, psi, psi_l, psi_r, B, n_suiv)
+            call build_A_N(t(i), dt, m, psi, psi_l, psi_r, B, A_N)
+            call build_b_N(t(i), dt, m, psi, n, psi_l, psi_r, n_l, n_r, B, b_N)
+            call linSolve("plu", m%l, A_N, b_N, n_suiv)
 
             ! calcul itéré p suivant
-            call iter_p(t(i), dt, m, p, p_r, p_l, psi, psi_l, psi_r, B, p_suiv)
+            call build_A_P(t(i), dt, m, psi, psi_l, psi_r, B, A_P)
+            call build_b_P(t(i), dt, m, psi, p, psi_l, psi_r, p_l, p_r, B, b_P)
+            call linSolve("plu", m%l, A_P, b_P, p_suiv)
 
             ! déduction itéré psi suivant
             call potentiel(m, n_suiv, p_suiv, ci, psi_l(t(i)), psi_r(t(i)), psi_suiv)
--- main.f95
+++ main.f95
@@ -12,7 +12,10 @@ PROGRAM main
 
     real(rp), dimension(:), allocatable :: x2, t
     real(rp) :: cfl
+    ! approx schéma explicite
     real(rp), dimension(:), allocatable :: approx_p, approx_n, approx_psi
+    ! approx schéma semi implicite
+    real(rp), dimension(:), allocatable :: approx_p_si, approx_n_si, approx_psi_si
 
     open(unit = 1, file = "entrees/constantes")
     read (1, *) n
@@ -29,12 +32,14 @@ PROGRAM main
     t = linspace(0.0_rp, Tf, m)
 
     allocate(approx_n(maill%l), approx_p(maill%l), approx_psi(maill%l))
+    allocate(approx_n_si(maill%l), approx_p_si(maill%l), approx_psi_si(maill%l))
 
 
 
     ! -------------------------------------------------------------------------------------------------------
     ! Schéma explicite
     ! -------------------------------------------------------------------------------------------------------
+    write (*, *) "Schéma explicite. Thinking..."
     call vf_explicite_dd(t, maill, B2, c_dopage, n0, p0, n_l, n_r, p_l, p_r, psi_l, psi_r, &
         approx_n, approx_p, approx_psi)
 
@@ -46,8 +51,25 @@ PROGRAM main
 
 
 
+    ! -------------------------------------------------------------------------------------------------------
+    ! Schéma explicite
+    ! -------------------------------------------------------------------------------------------------------
+    write (*, *) "Schéma semi-implicite. Thinking..."
+    call vf_semi_implicite_dd(t, maill, B2, c_dopage, n0, p0, n_l, n_r, p_l, p_r, psi_l, psi_r, &
+        approx_n_si, approx_p_si, approx_psi_si)
+
+    call saveSol(maill%x, (/ n_l(maill%x(1)), approx_n_si, n_r(maill%x(maill%l + 2)) /), &
+        "sorties/approx_n_si.dat")
+    call saveSol(maill%x, (/ p_l(maill%x(1)), approx_p_si, p_r(maill%x(maill%l + 2)) /), &
+        "sorties/approx_p_si.dat")
+    call saveSol(maill%x, (/ psi_l(maill%x(1)), approx_psi_si, psi_r(maill%x(maill%l + 2)) /), &
+        "sorties/approx_psi_si.dat")
+
+
+
     ! *******************************************************************************************************
     deallocate(approx_n, approx_p, approx_psi)
+    deallocate(approx_n_si, approx_p_si, approx_psi_si)
     call rmMesh(maill)
     deallocate(x2)
 
