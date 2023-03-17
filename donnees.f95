abd73afd48308d6cb53d8f189323d0df2b9f2940
vgitv <33827923+vgitv@users.noreply.github.com>
2023-03-17 11:15:47 +0100

    mise en page

--- donnees.f95
+++ donnees.f95
@@ -2,48 +2,16 @@ MODULE donnees
 
     use math
     use maillage
-    use variables
 
     implicit none
 
 contains
 
     ! =======================================================================================================
-    ! PROBLÈME DE POISSON
+    ! Schéma explicite
     ! =======================================================================================================
-
     ! -------------------------------------------------------------------------------------------------------
-    ! second membre du problème de Poisson
-    ! -------------------------------------------------------------------------------------------------------
-    function f(x)
-        ! paramètres
-        real(rp), intent(in) :: x
-
-        ! return
-        real(rp) :: f
-
-        f = 1.0_rp
-    end function
-
-
-
-    ! -------------------------------------------------------------------------------------------------------
-    ! Solution de l'équation de Poisson avec f = 1 et ul = ur = 0
-    ! -------------------------------------------------------------------------------------------------------
-    function u(x)
-        ! paramètres
-        real(rp), intent(in) :: x
-
-        ! return
-        real(rp) :: u
-
-        u = x * (1.0_rp - x) / 2.0_rp
-    end function
-
-
-
-    ! -------------------------------------------------------------------------------------------------------
-    ! Construction matrice A pour résolution A x = b dans le cas de l'équation de Poisson (idem éq thermique)
+    ! Construction matrice A_psi : A_psi psi = b_psi. La sol donne psi pour n et p donnés (temps fixé)
     ! -------------------------------------------------------------------------------------------------------
     subroutine build_A(m, A)
         ! paramètres
@@ -64,61 +32,6 @@ contains
 
 
 
-    ! -------------------------------------------------------------------------------------------------------
-    ! Construction vecteur b pour résolution A x = b dans le cas de l'équation de Poisson
-    ! -------------------------------------------------------------------------------------------------------
-    subroutine build_b(m, fc, ul, ur, b)
-        ! paramètres
-        type(Mesh), intent(in) :: m
-        real(rp), dimension(:), intent(out) :: b
-        real(rp), external :: fc
-        real(rp), intent(in) :: ul, ur
-
-        ! variables locales
-        integer :: i
-
-        do i = 1, m%l
-            b(i) = m%h(i) * f(m%x(i + 1))
-        end do
-
-        b(1) = b(1) + ul / m%h2(1)
-        b(m%l) = b(m%l) + ur / m%h2(m%l + 1)
-    end subroutine build_b
-
-
-
-    ! =======================================================================================================
-    ! NEWTON 1D
-    ! =======================================================================================================
-
-    ! -------------------------------------------------------------------------------------------------------
-    ! fonction test pour algo newton 1D : une fonction g et sa dérivée gp ( = gprime)
-    ! -------------------------------------------------------------------------------------------------------
-    function g(x)
-        ! paramètres
-        real(rp), intent(in) :: x
-
-        ! return
-        real(rp) :: g
-
-        g = (x - 1.0_rp) * (x + 1.0_rp) * (x - 3.0_rp)
-    end function
-
-    function gp(x)
-        ! paramètres
-        real(rp), intent(in) :: x
-
-        ! return
-        real(rp) :: gp
-
-        gp = (x + 1.0_rp) * (x - 3.0_rp) + (x - 1.0_rp) * (x - 3.0_rp) + (x - 1.0_rp) * (x + 1.0_rp)
-    end function
-
-
-
-    ! =======================================================================================================
-    ! Schéma explicite
-    ! =======================================================================================================
     ! -------------------------------------------------------------------------------------------------------
     ! Calcul second membre A_psi psi = b_psi. La sol donne psi pour n et p donnés (temps fixé)
     ! -------------------------------------------------------------------------------------------------------
@@ -173,6 +86,21 @@ contains
         call build_A(m, A_psi)
         call build_b_psi(m, n, p, c, psi_gauche, psi_droit, b_psi)
         call linSolve("plu", m%l, A_psi, b_psi, psi)
+    end subroutine
+
+
+
+    ! -------------------------------------------------------------------------------------------------------
+    ! Calcul le vecteur n au pas de temps suivant.
+    ! -------------------------------------------------------------------------------------------------------
+    subroutine iter_n(m, n, n_suiv)
+        ! paramètres
+        type(Mesh), intent(in) :: m
+        real(rp), dimension(:), intent(in) :: n
+        real(rp), dimension(:), intent(out) :: n_suiv
+
+        ! variables locales
+        !++!
     end subroutine
 
 END MODULE donnees
